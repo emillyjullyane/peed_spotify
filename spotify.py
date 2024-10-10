72e8413a6bd89d3c18e7971dc9cbb64f1485467f
Emilly Jullyane <131739717+emillyjullyane@users.noreply.github.com>
2024-10-10 14:55:53 -0300

    Add files via upload

--- spotify.py
+++ spotify.py
@@ -76,3 +76,77 @@ class Playlist:
             new_node.prev = current
             current.next.prev = new_node
             current.next = new_node
+
+
+# Método para exibir a playlist
+    def print_playlist(self):
+            current = self.head
+            if not current:
+                print("A playlist está vazia.")
+            else:
+                while current:
+                    print(current.data)
+                    current = current.next
+
+def main():
+    playlist = Playlist()
+    while True:
+        print('Escolha uma opçãao:\n')
+        print('1 - Adicionar música')
+        print('2 - Remover música')
+        print('3 - Mover música para outra posição')
+        print('4 - Listar playlist atual')
+        print('5 - Sair')
+
+        opcao = input('Escolha uma opção')
+
+        if opcao == '1':
+            print('---- Adicionar música na playlist ----\n')
+            nome = input("Digite o nome da música: \n")
+            artista = input("Digite o nome do artista: \n")
+            duracao = input("Digite a duração da música: \n")
+            nova_musica = Musica(nome, artista, duracao)
+            playlist.append(nova_musica)
+            print("Música adicionada com sucesso!\n")
+        
+        elif opcao == '2':
+            print('---- Remover música da playlist ----\n')
+            nome = input('Nome da música: \n')
+            artista = input ('Nome do artista: \n')
+            duracao = input ('Duração da música: \n')
+            musica_remover = Musica(nome, artista, duracao)
+            try:
+                playlist.remove(musica_remover)
+                print("Música removida com sucesso!")
+            except ValueError as e:
+                print('Erro ao remover música.')
+
+        elif opcao == '3':
+            print('---- Mover música para outra posição ----\n')   
+            nome = input('Nome da música: \n')
+            artista = input('Nome do artista: \n')
+            duracao = input('Duração da música: \n')
+            nova_posicao = int(input("Digite a nova posição (0-index): \n"))
+            musica_mover = Musica(nome, artista, duracao)
+            try:
+                playlist.remove(musica_mover)
+                playlist.insert(nova_posicao, musica_mover)
+                print('Música movida com sucesso!')
+            except ValueError as e:
+                print('Erro ao mover música.')
+            except IndexError as e:
+                print('Erro ao mover música.')
+
+        elif opcao == '4':
+            print("Playlist atual: ")
+            playlist.print_playlist()
+
+        elif opcao == '5':
+            print("Encerrado.")
+            break
+
+        else:
+            print("Opção inválida. Tente novamente.")
+
+if __name__ == "__main__":
+    main()
